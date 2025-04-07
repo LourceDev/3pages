9c1b3ecf08ea8841f62617275419caea4ee11acf
Shubh A Chudasama <shubh.cs@proton.me>
2025-04-07 21:45:57 +0530

    server: exit if db connection fails

--- server/src/main.ts
+++ server/src/main.ts
@@ -9,7 +9,11 @@ import { startServer } from "./server";
 async function main() {
   const logger = newLogger("main");
 
-  await connectDb();
+  if (null === (await connectDb())) {
+    logger.error("Failed to connect to the database. Exiting...");
+    return;
+  }
+
   const app = startServer();
   app.listen(env.PORT, () => {
     logger.info(`Server is running on http://localhost:${env.PORT}`);
